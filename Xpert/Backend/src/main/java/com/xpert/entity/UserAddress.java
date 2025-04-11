6dc883350ba080051bbfcd85dda50c085876f50b
Ashik <nasirhasan.ashik@gmail.com>
2025-04-11 16:05:03 +0600

    Handled @Builder.Default warning in UserAddress entity

--- Xpert/Backend/src/main/java/com/xpert/entity/UserAddress.java
+++ Xpert/Backend/src/main/java/com/xpert/entity/UserAddress.java
@@ -1,11 +1,16 @@
 package com.xpert.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "user_addresses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserAddress {
 
     @Id
@@ -37,103 +42,7 @@ public class UserAddress {
     @Column(name = "is_default", nullable = false)
     private Boolean isDefault;
 
+    @Builder.Default
     @Column(name = "is_deleted", nullable = false)
     private Boolean isDeleted = false;
-
-    // Constructors
-    public UserAddress() {}
-
-    public UserAddress(String title, Users user, String country, String city, String state, String zipCode,
-                       String streetAddress, Boolean isDefault, Boolean isDeleted) {
-        this.title = title;
-        this.user = user;
-        this.country = country;
-        this.city = city;
-        this.state = state;
-        this.zipCode = zipCode;
-        this.streetAddress = streetAddress;
-        this.isDefault = isDefault;
-        this.isDeleted = isDeleted;
-    }
-
-    // Getters and Setters
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Users getUser() {
-        return user;
-    }
-
-    public void setUser(Users user) {
-        this.user = user;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getStreetAddress() {
-        return streetAddress;
-    }
-
-    public void setStreetAddress(String streetAddress) {
-        this.streetAddress = streetAddress;
-    }
-
-    public Boolean getIsDefault() {
-        return isDefault;
-    }
-
-    public void setIsDefault(Boolean isDefault) {
-        this.isDefault = isDefault;
-    }
-
-    public Boolean getIsDeleted() {
-        return isDeleted;
-    }
-
-    public void setIsDeleted(Boolean isDeleted) {
-        this.isDeleted = isDeleted;
-    }
 }
